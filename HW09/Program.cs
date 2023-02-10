0c4b4bea22acc85cd3b64a0ff1fc4d57d62a1121
cys_jpt <aichenyansen@icloud.com>
2023-02-10 08:28:57 +0800

    update

---
+++ HW09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW09
+{
+
+    /// <summary>
+    ///  工号不够用了怎么办
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
